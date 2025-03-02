8b036519ebe779a098a7de39b6852ad3f6f269fb
Haseeblp12 <134800820+Haseeblp12@users.noreply.github.com>
2025-03-02 13:39:56 +0500

    Add files via upload

---
+++ script .js
@@ -0,0 +1,5 @@
+document.getElementById("gift").addEventListener("click", function() {
+    this.style.display = "none"; // Hide the gift box
+    document.getElementById("message").style.display = "block"; // Show message
+});
+
